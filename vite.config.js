9db73ccf2f0cf3e8fb9a281433c70f507f8fc005
Cesar Martinez <cesarj4114@gmail.com>
2024-04-08 16:02:52 -0400

    fix: remove engine selection

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/portfolio_monorepo/",
+  base: "./",
   test: {
     environment: "happy-dom",
     setupFiles: ["./.vitest/test-setup.js"],
