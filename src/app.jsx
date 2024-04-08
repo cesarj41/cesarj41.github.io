7d3398b882d02ae326d3a9048f39fd07625518c3
Cesar Martinez <cesarj4114@gmail.com>
2024-04-08 17:10:35 -0400

    chore: testing without react router

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,19 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import { ChallengeLayout } from "./components/challenge-layout";
-import { LandingPage, QRCodeChallengePage } from "./pages";
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" index element={<LandingPage />} />
-      <Route
-        path="challenges"
-        element={
-          <ChallengeLayout>
-            <Outlet />
-          </ChallengeLayout>
-        }
-      >
-        <Route path="qrcode" element={<QRCodeChallengePage />} />
-      </Route>
-    </Routes>
+    // <Routes>
+    //   <Route path="/" index element={<LandingPage />} />
+    //   <Route
+    //     path="challenges"
+    //     element={
+    //       <ChallengeLayout>
+    //         <Outlet />
+    //       </ChallengeLayout>
+    //     }
+    //   >
+    //     <Route path="qrcode" element={<QRCodeChallengePage />} />
+    //   </Route>
+    // </Routes>
+    <div>Testing without react router</div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import { render } from "preact";
-import { BrowserRouter } from "react-router-dom";
 import { App } from "./app.jsx";
 import "./assets/css/root.css";
 
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("app")
-);
+render(<App />, document.getElementById("app"));
