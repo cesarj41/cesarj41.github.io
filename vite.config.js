e9ee60bea68c73222b873fdeb0997c62ee4c2849
Cesar Martinez <cesarj4114@gmail.com>
2024-04-08 14:39:48 -0400

    fix: corrected wrong repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/<REPO>/",
+  base: "/portfolio_monorepo/",
   test: {
     environment: "happy-dom",
     setupFiles: ["./.vitest/test-setup.js"],
