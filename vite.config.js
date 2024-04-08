7d383f05ed31d1d0e4e1f7bd94e02f64593703ee
Cesar Martinez <cesarj4114@gmail.com>
2024-04-08 15:28:29 -0400

    fix: rename base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "./",
+  base: "/portfolio_monorepo/",
   test: {
     environment: "happy-dom",
     setupFiles: ["./.vitest/test-setup.js"],
