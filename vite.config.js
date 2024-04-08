c70b9e3931d173a6363c66b5c875329e870241a0
Cesar Martinez <cesarj4114@gmail.com>
2024-04-08 14:27:14 -0400

    fix: vite config deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: "/<REPO>/",
   test: {
     environment: "happy-dom",
     setupFiles: ["./.vitest/test-setup.js"],
