2cbb9b5b2abb22fbfb8cfa8602319f09c66f97ed
Cesar Martinez <cesarj4114@gmail.com>
2024-05-16 02:14:25 -0400

    feat: added landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        theme: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+          input: "var(--input)",
+          primary: "var(--primary)",
+          "primary-foreground": "var(--primary-foreground)",
+          "primary-light": "var(--primary-light)",
+          secondary: "var(--secondary)",
+          "secondary-foreground": "var(--secondary-foreground)",
+          muted: "var(--muted)",
+          "muted-foreground": "var(--muted-foreground)",
+          accent: "var(--accent)",
+          "accent-foreground": "var(--accent-foreground)",
+          destructive: "var(--destructive)",
+          "destructive-foreground": "var(--destructive-foreground)"
+        }
+      }
+    }
   },
   plugins: []
 };
